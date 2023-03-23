65d6fb5fc89a9f129a7f45c223f23754e6e33615
Isaac Cha <ijcha93@gmail.com>
2023-03-22 22:05:22 -0400

    fix

--- assets/weather.js
+++ assets/weather.js
@@ -14,7 +14,7 @@ var windData;
 // get weather data when a city value is input into the search bar
 async function onSubmit() {
   // if statement to check if city input, check in, and check out all have values
-  if (cityInput.value !== "") {
+  if (cityInput.value !== "" && checkInInput.value !== "" && checkOutInput.value !== "") {
       // remove event listener to prevent duplicate elements
     searchBtn.removeEventListener("click", onSubmit);
     var weatherApi =
@@ -119,4 +119,18 @@ function currentWeatherCard() {
   weatherDiv.append(currentDiv);
 }
 
+// remove jumbotron when search is made
+function removeJumbo() {
+  searchBtn.removeEventListener("click", removeJumbo);
+  var displayToggel = document.getElementById("display");
+  displayToggel.style.display = "none";
+}
+
 searchBtn.addEventListener(`click`, onSubmit);
+searchBtn.addEventListener(`click`, removeJumbo);
+homeBtn.addEventListener("click", refreshPage);
+
+// refresh the page
+function refreshPage() {
+  location.reload();
+}
